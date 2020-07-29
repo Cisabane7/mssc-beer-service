15ac7f3032edbe46a9514fcdc1bd7394aebcc1c1
cisabane7 <chaste.isabane@gmail.com>
2020-07-29 19:31:57 +0200

    Add Brewing Listener for Low Inventory

--- src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
+++ src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
@@ -1,16 +1,15 @@
 package guru.springframework.msscbeerservice.events;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
-import lombok.Builder;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 
 @Data
-@RequiredArgsConstructor
+@AllArgsConstructor
 @Builder
+@NoArgsConstructor
 public class BeerEvent implements Serializable {
     static final long serialVersionUID = -5084215099189811504L;
-    private final BeerDto beer;
+    private BeerDto beer;
 }
